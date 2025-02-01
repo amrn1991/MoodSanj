33882e082a9f5d6b888116cb3204d2f1fa861285
amrn1991 <amrfst91@gmail.com>
2025-02-01 11:01:52 +0330

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-black text-white flex justify-center items-center">
+      <div className="w-full max-w-[600px]">
+        <h1 className="text-6xl">امروز مودت چطوره؟</h1>
+        <p className="text-2xl text-white/60">ثبت لحظات خوب و بدت رو به ما بسپار</p>
+        <div>
+          <button>شروع کن</button>
+        </div>
+      </div>
+    </div>
+  );
+}
